894fec2275729f971cabbaf2efbd37c0ea9a96a8
zabih ullah <zabihullah18381@gmail.com>
2025-10-07 13:26:57 +0500

    remove file

--- script.js
+++
@@ -1,8 +0,0 @@
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EST1F3YMY4"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-EST1F3YMY4');
-    </script>
